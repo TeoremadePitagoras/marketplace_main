79cd1fcb3ea86faf5ebf5f40153d37dca42f92e3
TeoremadePitagoras <nieblasespinozaangeladang23@cbtis116.edu.mx>
2025-11-11 12:15:15 -0800

    Login Medio Funcional

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import home, contact, detail
+from django.contrib.auth import views as auth_views
+from .views import home, contact, detail, register
+
+from .forms import LoginForm
 
 urlpatterns = [
-    path('', home, name='home'),
+    path ('', home, name='home'),
     path('contact/', contact, name='contact'),
-    path('detail/<int:pk>/', detail, name='detail'),
+    path('register/', register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='store/login.html', authentication_form=LoginForm)),
+    path('detail/<int:pk>/', detail, name='detail')
 ]
